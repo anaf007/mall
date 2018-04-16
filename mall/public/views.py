76cb3837816b1a0dcd22e2d8cb1cf1a2fe48821f
anngle <anaf@anafdeMacBook.local>
2018-04-17 00:05:45 +0800

    change public views test wechat

--- mall/public/views.py
+++ mall/public/views.py
@@ -32,6 +32,8 @@ def load_user(user_id):
 def home():
     """Home page."""
 
+    wechat.message.send_text('oN-VsuBVuhUf-HKYtiiqrwC5zlrg','msg_title')
+
     follow = Follow.query.filter_by(users=current_user).all()
     len_follow = len(follow)
     if len_follow>0 and len_follow<=1:
