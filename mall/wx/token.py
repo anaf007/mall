f389883426d5a3b6cc62c3c2aa7c46234e651beb
anngle <anaf@anafdeMacBook.local>
2018-04-18 03:02:12 +0800

    test wechat msg

--- mall/wx/token.py
+++ mall/wx/token.py
@@ -77,7 +77,7 @@ def token_post():
             if users_order:
                 redirect_url = url_for('store.show_order',id=users_order[0].id,_external=True)
 
-                textreply_str = f'您<a href="{redirect_url}">点击查看订单详情。</a>'
+                textreply_str = f'<a href="{redirect_url}">点击查看订单详情。</a>'
                 
             else:
                 textreply_str = '输入错误'
