12990c40e0414bf634bccf5552610672e1da2fe1
anngle <anaf@anafdeMacBook.local>
2018-04-18 01:07:13 +0800

    test wechat

--- mall/wx/token.py
+++ mall/wx/token.py
@@ -76,7 +76,8 @@ def token_post():
 
             if users_order:
                 redirect_url = url_for('store.show_order',id=users_order[0].id)
-                textreply_str = '您有新的销售订单。<a href="redirect_url">点击查看</a>'
+                print redirect_url
+                textreply_str = '您有新的销售订单。<a href="{redirect_url}">点击查看</a>'
                 
             else:
                 textreply_str = '输入错误'
