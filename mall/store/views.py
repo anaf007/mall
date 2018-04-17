b05c795eb06bec7c0fb6c4e2b447b11cfd426939
anngle <anaf@anafdeMacBook.local>
2018-04-18 01:54:13 +0800

    test wechat

--- mall/store/views.py
+++ mall/store/views.py
@@ -27,7 +27,7 @@ blueprint = Blueprint('store', __name__, url_prefix='/store')
 def home():
 
     redirect_url = url_for('store.show_order',id=1,_external=True)
-    textreply_str = '您有新的销售订单。<a href="{redirect_url}">点击查看</a>'
+    textreply_str = f'您有新的销售订单。<a href="{redirect_url}">点击查看</a>'
     print(textreply_str)
 
 
