958f7dcedeae07ab3b64b7e94c5d60b4ac2f0c4d
anngle <anaf@anafdeMacBook.local>
2018-05-10 15:16:13 +0800

    change store home

--- mall/store/views.py
+++ mall/store/views.py
@@ -50,7 +50,7 @@ def home():
 
     res = wechat.qrcode.create({
         'expire_seconds': 1800,
-        'action_name': 'QR_SCENE',
+        'action_name': 'QR_LIMIT_SCENE',
         'action_info': {
             'scene': {'scene_id':store.id },
             }
