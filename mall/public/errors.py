42aa782c7a00912c14ec3566fd818c0886f03396
anngle <anaf@163.com>
2018-06-06 22:49:46 +0800

    增加500错误发送邮件提醒

--- mall/public/errors.py
+++ mall/public/errors.py
@@ -4,12 +4,12 @@ from . import blueprint
 from flask import flash,render_template
 from mall.utils import send_email
 from log import logger
+from mall.extensions import executor
 
 @blueprint.errorhandler(500)
 def internal_server_error(e):
     logger.error(e)
     executor.submit(send_email,f'500错误{e}')
-   
 	return render_template('500.html'), 500
 
 @blueprint.errorhandler(404)
